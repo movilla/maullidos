8d34604163337e9a42b38c045d824b9a0abaf218
movilla <moviglez@gmail.com>
2014-06-18 21:30:59 +0200

    resolver licencias

---
+++ js/app.js
@@ -0,0 +1,118 @@
+var b01a = document.querySelector("#b01a");
+if (b01a) {
+    b01a.onclick = function () {
+        document.getElementById("sonido_1").play();
+	false
+	};
+}
+var b01b = document.querySelector("#b01b");
+if (b01b) {
+    b01b.onclick = function () {
+        document.getElementById("sonido_1").stop(); 
+	};
+}
+
+var b02a = document.querySelector("#b02a");
+var b02b = document.querySelector("#b02b");
+if (b02a) {
+    b02a.onclick = function () {
+        document.getElementById("sonido_2").play(); 
+	};
+}
+if (b02b) {
+    b02b.onclick = function () {
+        document.getElementById("sonido_2").pause(); 
+	};
+}
+
+var b03a = document.querySelector("#b03a");
+if (b03a) {
+    b03a.onclick = function () {
+        document.getElementById("sonido_3").play(); 
+	};
+}
+var b03b = document.querySelector("#b03b");
+if (b03b) {
+    b03b.onclick = function () {
+        document.getElementById("sonido_3").pause(); 
+	};
+}
+
+var b04a = document.querySelector("#b04a");
+if (b04a) {
+    b04a.onclick = function () {
+        document.getElementById("sonido_4").play(); 
+	};
+}
+var b04b = document.querySelector("#b04b");
+if (b04b) {
+    b04b.onclick = function () {
+        document.getElementById("sonido_4").pause(); 
+	};
+}
+
+var b05a = document.querySelector("#b05a");
+var b05b = document.querySelector("#b05b");
+if (b05a) {
+    b05a.onclick = function () {
+        document.getElementById("sonido_5").play(); 
+	};
+}
+if (b05b) {
+    b05b.onclick = function () {
+        document.getElementById("sonido_5").pause(); 
+	};
+}
+
+var b06a = document.querySelector("#b06a");
+var b06b = document.querySelector("#b06b");
+if (b06a) {
+    b06a.onclick = function () {
+        document.getElementById("sonido_6").play(); 
+	};
+}
+if (b06b) {
+    b06b.onclick = function () {
+        document.getElementById("sonido_6").pause(); 
+	};
+}
+
+var b07a = document.querySelector("#b07a");
+if (b07a) {
+    b07a.onclick = function () {
+        document.getElementById("sonido_7").play(); 
+	};
+}
+var b07b = document.querySelector("#b07b");
+if (b07b) {
+    b07b.onclick = function () {
+        document.getElementById("sonido_7").pause(); 
+	};
+}
+
+var b08a = document.querySelector("#b08a");
+var b08b = document.querySelector("#b08b");
+if (b08a) {
+    b08a.onclick = function () {
+        document.getElementById("sonido_8").play(); 
+	};
+}
+
+if (b08b) {
+    b08b.onclick = function () {
+        document.getElementById("sonido_8").pause(); 
+	};
+}
+
+var b09a = document.querySelector("#b09a");
+var b09b = document.querySelector("#b09b");
+if (b09a) {
+    b09a.onclick = function () {
+        document.getElementById("sonido_9").play(); 
+	};
+}
+if (b09b) {
+    b09b.onclick = function () {
+        document.getElementById("sonido_9").pause(); 
+	};
+}
